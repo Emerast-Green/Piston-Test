74657fec469c8e782acae482709113211f80a62a
Emerast-Green <kamil.sienkowski777@gmail.com>
2023-05-02 10:26:27 +0200

    2.1-2

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@ use piston::{ButtonEvent, ButtonState, Key, Button, EventLoop};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+
 use glem::*;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    physics: Vec<Physics>,
-    colliders: Vec<Collider>,
-    variables: Vec<f64>,
+    game: Game,
     glyphs: GlyphCache<'static>,
 }
 
@@ -30,17 +29,17 @@ impl App {
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(BLACK, gl);
-            for i in &self.colliders {
+            for i in &self.game.colliders {
                 i.draw(c,gl);
             }
-            for i in &self.physics {
+            for i in &self.game.physics {
                 i.draw(c,gl);
             }
             let transform = c
             .transform
             .trans(10.0,10.0);
             Text{color:[1.0,1.0,0.0,1.0],font_size:16,round:false}.draw(
-                self.colliders[0].collides(&self.physics[0]).to_string().as_str(), 
+                self.game.colliders[0].collides(&self.game.physics[0]).to_string().as_str(), 
                 &mut self.glyphs, 
                 &c.draw_state, 
                 transform, 
@@ -49,8 +48,8 @@ impl App {
     }
 
     fn update(&mut self, _args: &UpdateArgs) {
-        for i in &mut self.physics {
-            i.update(&self.colliders);
+        for i in &mut self.game.physics {
+            i.update(&self.game.colliders);
         }
 
     }
@@ -61,7 +60,8 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create a Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-squares", [200, 200])
+    let window_size = [640.0,480.0];
+    let mut window: Window = WindowSettings::new("moving-squares", window_size)
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -71,17 +71,17 @@ fn main() {
     const F: () = ();
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        physics: Vec::new(),
-        colliders: Vec::new(),
-        variables: Vec::new(),
+        game: Game::new(),
         glyphs: GlyphCache::from_font(get_font(),F, TextureSettings::new()),
     };
     // VARIABLES
-    app.variables.push(30.0); // speed
-    app.physics.push(Physics::new((0.0,0.0),(30.0,30.0),20.0));
+    app.game.variables.push(30.0); // speed
+    app.game.physics.push(Physics::new((0.0,0.0),(30.0,30.0),20.0));
     //app.physics[0].controller.motion[3]=true;
-    app.colliders.push(Collider::new((0.0,180.0),(200.0,20.0)));
-    app.colliders.push(Collider::new((60.0,120.0),(50.0,1.0)));
+    app.game.colliders.push(Collider::new((0.0,window_size[1]-20.0),(window_size[0],20.0))); // floor 
+    app.game.colliders.push(Collider::new((60.0,120.0),(50.0,1.0))); // platform
+    app.game.colliders.push(Collider::new((-10.0,0.0),(10.0,window_size[1]))); // left border
+    app.game.colliders.push(Collider::new((window_size[0],0.0),(10.0,window_size[1]))); // right border
     let mut events = Events::new(EventSettings::new()).max_fps(30);
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
@@ -97,9 +97,9 @@ fn main() {
                 match k.button {
                     //Button::Keyboard(Key::W) => {app.physics[0].controller.motion[0]=true;},
                     //Button::Keyboard(Key::S) => {app.physics[0].controller.motion[1]=true;},
-                    Button::Keyboard(Key::A) => {app.physics[0].controller.motion[2]=true;},
-                    Button::Keyboard(Key::D) => {app.physics[0].controller.motion[3]=true;},
-                    Button::Keyboard(Key::Space) => {app.physics[0].controller.jump=true;},
+                    Button::Keyboard(Key::A) => {app.game.physics[0].controller.motion[2]=true;},
+                    Button::Keyboard(Key::D) => {app.game.physics[0].controller.motion[3]=true;},
+                    Button::Keyboard(Key::Space) => {app.game.physics[0].controller.jump=true;},
                     Button::Keyboard(Key::Escape) => {println!("Handled Keyboard(Escape) was pressed!");},
                     _ => {println!("Unhandled {:?} was pressed!",k.button)},
                 }
@@ -108,8 +108,8 @@ fn main() {
                 match k.button {
                     //Button::Keyboard(Key::W) => {app.physics[0].controller.motion[0]=false;},
                     //Button::Keyboard(Key::S) => {app.physics[0].controller.motion[1]=false;},
-                    Button::Keyboard(Key::A) => {app.physics[0].controller.motion[2]=false;},
-                    Button::Keyboard(Key::D) => {app.physics[0].controller.motion[3]=false;},
+                    Button::Keyboard(Key::A) => {app.game.physics[0].controller.motion[2]=false;},
+                    Button::Keyboard(Key::D) => {app.game.physics[0].controller.motion[3]=false;},
                     Button::Keyboard(Key::Escape) => {},
                     _ => {},
                 }
